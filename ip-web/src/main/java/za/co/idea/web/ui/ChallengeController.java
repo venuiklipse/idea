be424ee00d93ef30d4b3b7dc1557c42534a86236
venuiklipse <venu@iklipse.co.za>
2014-04-10 18:10:44 +0200

    update

---
+++ ip-web/src/main/java/za/co/idea/web/ui/ChallengeController.java
@@ -0,0 +1,25 @@
+package za.co.idea.web.ui;
+
+import java.io.Serializable;
+
+public class ChallengeController implements Serializable {
+
+	private static final long serialVersionUID = -6939719926402016671L;
+
+	public String showCreateChallenge() {
+		return "";
+	}
+
+	public String showViewChallenge() {
+		return "";
+	}
+
+	public String showCreateSolution() {
+		return "";
+	}
+
+	public String showViewSolution() {
+		return "";
+	}
+
+}
